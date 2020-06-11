df3ece111d0ade095df84c0c1bb17b8c0e1d41c6
Sansossio <juliosansossio@gmail.com>
2020-06-11 20:40:19 +0200

    Local bitcoin

---
+++ src/services/localbitcoin/localbitcoin.service.ts
@@ -0,0 +1,9 @@
+import { BaseService } from '../base/base.service'
+import { config } from '../dolartoday/dolartoday.config'
+
+export class LocalBitcoinService extends BaseService {
+  static async getPrice () {
+    const data = await this.getData(config.API_URL)
+    return data.USD.localbitcoin_ref as number
+  }
+}
