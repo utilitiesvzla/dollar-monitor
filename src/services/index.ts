8bae17bc3cb01e8c45942d35443ea618067af4ee
Sansossio <juliosansossio@gmail.com>
2020-06-11 16:36:57 +0200

    Export services

---
+++ src/services/index.ts
@@ -0,0 +1,3 @@
+export { DolartodayService } from './dolartoday/dolartoday.service'
+export { BolivarCucutaService } from './bolivar-cucuta/bolivar-cucuta.service'
+export { YadioIOService } from './yadio-io/yadio-io.service'
