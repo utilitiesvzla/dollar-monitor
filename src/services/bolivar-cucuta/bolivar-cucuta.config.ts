7d747a16f2989eef9d6b0e90a725b4b5b16eb2ea
Sansossio <juliosansossio@gmail.com>
2020-06-12 13:30:08 +0200

    Refactor bolivarcucuta

--- src/services/bolivar-cucuta/bolivar-cucuta.config.ts
+++ src/services/bolivar-cucuta/bolivar-cucuta.config.ts
@@ -1,5 +1,6 @@
 import { IBaseConfig } from '../base/base.config'
 
 export const config: IBaseConfig = {
-  API_URL: 'https://s3.amazonaws.com/frontcloud/data.json'
+  API_URL: 'https://s3.amazonaws.com/frontcloud/data.json',
+  PATH: 'USDVEF.dolarcucuta_efe'
 }
--- src/services/bolivar-cucuta/bolivar-cucuta.service.ts
+++ src/services/bolivar-cucuta/bolivar-cucuta.service.ts
@@ -2,8 +2,5 @@ import { config } from './bolivar-cucuta.config'
 import { BaseService } from '../base/base.service'
 
 export class BolivarCucutaService extends BaseService {
-  static async getPrice () {
-    const data = await this.getData(config.API_URL)
-    return data.USDVEF.dolarcucuta_efe as number
-  }
+  protected static config = config
 }
