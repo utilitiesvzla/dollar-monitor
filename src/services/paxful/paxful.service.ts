8302420016132e0e2790dcdd44a781f4e686756b
Sansossio <juliosansossio@gmail.com>
2020-06-12 13:28:24 +0200

    Refactor - Paxful

--- src/services/paxful/paxful.service.ts
+++ src/services/paxful/paxful.service.ts
@@ -2,8 +2,5 @@ import { config } from './paxful.config'
 import { BaseService } from '../base/base.service'
 
 export class PaxfulService extends BaseService {
-  static async getPrice () {
-    const data = await this.getData(config.API_URL)
-    return data.data.rate.usd as number
-  }
+  protected static config = config
 }
