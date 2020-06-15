b880307598c7b805b3b6704f46aafb3e79000524
Sansossio <juliosansossio@gmail.com>
2020-06-15 22:28:43 +0200

    Ignore petro service to average

--- src/average/average.ts
+++ src/average/average.ts
@@ -1,11 +1,14 @@
 import * as dollarServices from '../services'
+import { BaseService } from '../services/base/base.service'
+
+const PETRO_NAME = dollarServices.PetroService.name
 
 export async function getAverage () {
-  const services = Object.keys(dollarServices)
-  let total = 0
-  for (const service of services) {
-    total += await dollarServices[service].getPrice()
-  }
-  const average = total / services.length
+  const services = Object.keys(dollarServices).filter(name => name !== PETRO_NAME)
+  const total = await Promise.all(services.map((serviceName) => {
+    const service = dollarServices[serviceName] as typeof BaseService
+    return service.getPrice()
+  }))
+  const average = total.reduce((a, b) => a + b, 0) / services.length
   return average
 }
