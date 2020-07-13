81c80dc0f86b7b015f68a0546054960cdda80cc9
Julio Sansossio <J.Sansossio@payvision.com>
2020-07-13 15:39:55 +0200

    Fix akbfintech rate

--- src/services/akbfintech/akbfintech.service.ts
+++ src/services/akbfintech/akbfintech.service.ts
@@ -3,4 +3,9 @@ import { BaseService } from '../base/base.service'
 
 export class AKBfintechService extends BaseService {
   protected static config = config
+
+  static async getPrice () {
+    const value = await super.getPrice()
+    return 1 / value
+  }
 }
