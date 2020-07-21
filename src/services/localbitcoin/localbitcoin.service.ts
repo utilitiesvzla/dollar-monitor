b6a71782dcaadcc31c60344fcbeaf9d8b6f8a646
Julio Sansossio <J.Sansossio@payvision.com>
2020-07-21 16:11:52 +0200

    Use exchange monitor

--- src/services/localbitcoin/localbitcoin.service.ts
+++ src/services/localbitcoin/localbitcoin.service.ts
@@ -1,16 +1,6 @@
-import { BaseService } from '../base/base.service'
 import { config } from './localbitcoin.config'
-import { DolartodayBtcService } from '../dolartoday-btc/dolartoday-btc.service'
+import { ExchangemonitorService } from '../exchangemonitor/exchangemonitor.service'
 
-const VALUE_KEY = 'avg_12h'
-
-export class LocalBitcoinService extends BaseService {
-  static async getPrice () {
-    const data = await this.getData(config.API_URL)
-
-    const localBitcoinValue = +data.VES[VALUE_KEY] / +data.USD[VALUE_KEY]
-    const dolarTodayBtcValue = await DolartodayBtcService.getPrice()
-
-    return Math.max(localBitcoinValue, dolarTodayBtcValue)
-  }
+export class LocalBitcoinService extends ExchangemonitorService {
+  static config = config
 }
