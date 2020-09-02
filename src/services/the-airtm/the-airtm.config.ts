049bc6439108d7ad22cf67e2cd11b916f19ad333
Julio Sansossio <J.Sansossio@payvision.com>
2020-09-02 16:39:20 +0200

    Update airmtm service to monitorexchange

--- src/services/the-airtm/the-airtm.config.ts
+++ src/services/the-airtm/the-airtm.config.ts
@@ -1,6 +1,5 @@
 import { IBaseConfig } from '../base/base.config'
 
 export const config: IBaseConfig = {
-  API_URL: 'https://rates.airtm.com/rates.json',
-  INFO_URL: 'https://rates.airtm.com'
+  API_URL: 'https://exchangemonitor.net/rates/ve/data/AirTM.json'
 }
--- src/services/the-airtm/the-airtm.service.ts
+++ src/services/the-airtm/the-airtm.service.ts
@@ -1,18 +1,6 @@
 import { config } from './the-airtm.config'
-import { BaseService } from '../base/base.service'
-import { JSDOM } from 'jsdom'
+import { ExchangemonitorService } from '../exchangemonitor/exchangemonitor.service'
 
-export class TheAirTMService extends BaseService {
-  private static async getToday () {
-    const websiteData = await this.getData(config.INFO_URL)
-    const html = new JSDOM(websiteData)
-    const element = html.window.document.getElementById('last_time')
-    return (element as any).defaultValue
-  }
-
-  static async getPrice () {
-    const d = await this.getToday()
-    const data = await this.getData(config.API_URL, { d })
-    return data.today?.VES_BANK?.buy || data.yesterday?.VES_BANK?.buy
-  }
+export class TheAirTMService extends ExchangemonitorService {
+  private config = config
 }
