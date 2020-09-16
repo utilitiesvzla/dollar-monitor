814ba601d517e9b89f37cb56c47054f21a90e66f
Julio Sansossio <J.Sansossio@payvision.com>
2020-09-16 15:16:02 +0200

    Fix paxful rate

--- src/services/paxful/paxful.service.ts
+++ src/services/paxful/paxful.service.ts
@@ -3,10 +3,4 @@ import { BaseService } from '../base/base.service'
 
 export class PaxfulService extends BaseService {
   protected static config = config
-
-  static async getPrice () {
-    const priceOne = await super.getPrice(config.API_URL)
-    const priceTwo = await super.getPrice(config.API_URL_ALT)
-    return ((priceOne + priceTwo) / 2) || priceOne || priceTwo
-  }
 }
