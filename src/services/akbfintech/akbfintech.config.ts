406621b76aca72cfce30a2855a43e6ff09cc3c76
Sansossio <juliosansossio@gmail.com>
2020-09-21 19:27:53 +0200

    alt api afkfintech

--- src/services/akbfintech/akbfintech.config.ts
+++ src/services/akbfintech/akbfintech.config.ts
@@ -1,5 +1,6 @@
 import { IBaseConfig } from '../base/base.config'
 
 export const config: IBaseConfig = {
-  API_URL: 'https://americankryptosbank.com/get-price?amount=1&receiving=0&sender=USD&receiver=VES&sender_country=United+States&receiver_country=Venezuela'
+  API_URL: 'https://americankryptosbank.com/get-price?amount=1&receiving=0&sender=USD&receiver=VES&sender_country=United+States&receiver_country=Venezuela',
+  API_ALT: 'https://akbfintech.com/get-price?amount=1&receiving=0&sender=USD&receiver=VES&sender_country=United+States&receiver_country=Venezuela'
 }
--- src/services/akbfintech/akbfintech.service.ts
+++ src/services/akbfintech/akbfintech.service.ts
@@ -4,8 +4,17 @@ import { BaseService } from '../base/base.service'
 export class AKBfintechService extends BaseService {
   protected static config = config
 
+  private static async altPrice () {
+    const data = await AKBfintechService.getData(AKBfintechService.config.API_ALT)
+    return data['0']
+  }
+
   static async getPrice () {
-    const data = await AKBfintechService.getData(AKBfintechService.config.API_URL)
-    return data.original
+    try {
+      const data = await AKBfintechService.getData(AKBfintechService.config.API_URL)
+      return data.original
+    } catch (e) {
+      return this.altPrice()
+    }
   }
 }
