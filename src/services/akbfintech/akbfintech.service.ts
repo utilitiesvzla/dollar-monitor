e7a2296dd1eff86bcbfbb7202afdcf64a5bbdd62
Sansossio <juliosansossio@gmail.com>
2020-09-21 19:50:19 +0200

    add overprice because of inflation

--- src/services/akbfintech/akbfintech.service.ts
+++ src/services/akbfintech/akbfintech.service.ts
@@ -1,18 +1,21 @@
 import { config } from './akbfintech.config'
 import { BaseService } from '../base/base.service'
 
+const MIN = 3.5
+const MAX = 5.7
+
 export class AKBfintechService extends BaseService {
   protected static config = config
 
   private static async altPrice () {
     const data = await AKBfintechService.getData(AKBfintechService.config.API_ALT)
-    return data['0']
+    return +data['0'] * this.getOverPrice(MIN, MAX)
   }
 
   static async getPrice () {
     try {
       const data = await AKBfintechService.getData(AKBfintechService.config.API_URL)
-      return data.original
+      return +data.original * this.getOverPrice(MIN, MAX)
     } catch (e) {
       return this.altPrice()
     }
--- src/services/localbitcoin/localbitcoin.service.ts
+++ src/services/localbitcoin/localbitcoin.service.ts
@@ -1,5 +1,17 @@
 import { config } from './localbitcoin.config'
 import { BaseService } from '../base/base.service'
+import * as _ from 'lodash'
+
+const INFORMATION_KEYS = [
+  'rates.last',
+  'avg_1h',
+  'avg_6h',
+  'avg_12h',
+  'avg_24h'
+]
+
+const MIN = 4.5
+const MAX = 5.2
 
 export class LocalBitcoinService extends BaseService {
   static config = config
@@ -10,6 +22,14 @@ export class LocalBitcoinService extends BaseService {
       USD,
       VES
     } = data
-    return +(VES.avg_1h || VES.avg_12h) / +USD.avg_24h
+    const allPrices: { key: string, price: number }[] = []
+    for (const key of INFORMATION_KEYS) {
+      const price = (+(_.get(VES, key) || _.get(VES, 'avg_12h')) / +_.get(USD, key)) * this.getOverPrice(MIN, MAX)
+      allPrices.push({
+        key,
+        price
+      })
+    }
+    return Math.max(...allPrices.map(({ price }) => price))
   }
 }
