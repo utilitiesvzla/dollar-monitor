8149a630ee11910a5cb3809fee5ef2f426b0c9d0
Sansossio <juliosansossio@gmail.com>
2020-10-07 19:30:07 +0200

    fix bcv rate

--- src/services/bcv/bcv.config.ts
+++ src/services/bcv/bcv.config.ts
@@ -1,5 +1,5 @@
 import { IBaseConfig } from '../base/base.config'
 
 export const config: IBaseConfig = {
-  API_URL: 'http://www.bcv.org.ve/estadisticas/tipo-de-cambio'
+  API_URL: 'http://www.bcv.org.ve'
 }
